7585dd47a52baa2a21f9b18b915e76a8e98c1b9f
Julian Lindblad <JulianLindblad@gmail.com>
2015-01-08 11:36:56 +0900

    Added a split function for splitting string and "split_convert_to" to split and convert the elements.

--- utility.cpp
+++ utility.cpp
@@ -1,6 +1,8 @@
 
 #include "utility.hpp"
 
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
 
 namespace shooter {
 
@@ -9,7 +11,25 @@ namespace shooter {
     return sf::milliseconds(ms);
   };
 
+  std::vector<std::string> split(const std::string& str, char delim) {
+    using namespace std;
 
+    vector<string> splitted;
+    boost::split(splitted, str, boost::is_any_of(string(1, delim)), boost::token_compress_on);
+
+    return splitted;
+  };
+
+  template<>
+  std::vector<int> split_convert_to<int>(const std::string& str, char delim) {
+    return split_convert_to<int>(str, delim, 
+        [](const std::string& s) { return std::stoi(s); } );
+  };
+  template<>
+  std::vector<float> split_convert_to<float>(const std::string& str, char delim) {
+    return split_convert_to<float>(str, delim, 
+        [](const std::string& s) { return std::stof(s); } );
+  };
 }
 
 
--- utility.hpp
+++ utility.hpp
@@ -3,10 +3,35 @@
 
 #include <SFML/System.hpp>
 
+#include <vector>
+#include <string>
+#include <functional>
+#include <iterator>
+
 namespace shooter {
 
   sf::Time operator "" _ms(unsigned long long ms);
 
+  std::vector<std::string> split(const std::string& str, char delim);
+
+  template<typename T, typename F>
+  std::vector<T> split_convert_to(const std::string& str, char delim, F converter) {
+    using namespace std;
+
+    vector<string> splitted = split(str, delim);
+
+    vector<T> result;
+    transform(begin(splitted), end(splitted), inserter(result, begin(result)), converter);
+
+    return result;
+  };
+
+  template<typename T> std::vector<T>
+    split_convert_to(const std::string& str, char delim);
+  template<> std::vector<int> 
+    split_convert_to<int>(const std::string& str, char delim);
+  template<> std::vector<float> 
+    split_convert_to<float>(const std::string& str, char delim);
 
 
 }
